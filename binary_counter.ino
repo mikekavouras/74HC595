2b626a9c6ed7e2e6e2d181b24cbeeb076bfd3412
Mike Kavouras <mikekavouras@github.com>
2020-07-04 13:14:55 -0400

    Clean up

--- binary_counter.ino
+++ binary_counter.ino
@@ -1,7 +1,6 @@
-int dataPin = 9;
-int clockPin = 8;
 int latchPin = 7;
-
+int clockPin = 8;
+int dataPin = 9;
 
 void setup() {
   pinMode(dataPin, OUTPUT);
@@ -13,8 +12,8 @@ void loop() {
   for (int numberToDisplay = 0; numberToDisplay < 256; numberToDisplay++) {
     digitalWrite(latchPin, LOW);
     shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay);  
-
     digitalWrite(latchPin, HIGH);
+
     delay(500);
   }
 }
--- binary_counter_spi.ino
+++ binary_counter_spi.ino
@@ -1,21 +1,25 @@
 #include <SPI.h>
 
-int ssPin = 7;
+int SSPin = 7;
 SPISettings settings(14000000, MSBFIRST, SPI_MODE0);
 
 void setup() {
   SPI.begin();
-  pinMode(ssPin, OUTPUT);
-  digitalWrite(ssPin, HIGH);
+
+  pinMode(SSPin, OUTPUT);
+  digitalWrite(SSPin, HIGH);
 }
 
 void loop() {
   for (int numberToDisplay = 0; numberToDisplay < 256; numberToDisplay++) {
-    digitalWrite(ssPin, LOW); 
+    digitalWrite(SSPin, LOW);
+
     SPI.beginTransaction(settings);
     SPI.transfer(numberToDisplay);
     SPI.endTransaction();
-    digitalWrite(ssPin, HIGH);
+
+    digitalWrite(SSPin, HIGH);
+
     delay(100);
   }
 }
